f7da099d4ecd800842108745138d7c9fa85a754c
tim-krebs <timkrebs9@gmail.com>
2021-12-28 18:00:07 +0100

    changed routers routing

---
+++ app/database.py
@@ -0,0 +1,37 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+
+
+from .config import settings
+
+#SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Z+eGmMKtb5hn@localhost/API'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# Connecting to databse
+    #while True:
+    #    try:
+    #        conn = psycopg2.connect(host='localhost', database='API', user='postgres', password='Z+eGmMKtb5hn', cursor_factory=RealDictCursor)
+    #        cursor = conn.cursor()
+    #        print("Database connected successfull!")
+    #        break
+    #    except Exception as error:
+    #        print("Connecting to database failed")
+    #        print("Error:", error)
+    #        time.sleep(5)
--- app/main.py
+++ app/main.py
@@ -1,85 +1,34 @@
-from operator import index
-from typing import Optional
-from random import randrange
-from fastapi import FastAPI, Response, status
-from fastapi.exceptions import HTTPException
-from fastapi.params import Body
-from pydantic import BaseModel
-from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from . import models
+from .database import engine
+from .routers import post, user, auth, like
+from .config import settings
+
+
+
+models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
 
-my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, 
-            {"title": "title of post 2", "content": "content of post 2", "id": 2},
-            {"title": "title of post 3", "content": "content of post 3", "id": 3}]
+origins = ["*"]
 
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-    rating: Optional[int] = None
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-
-def find_post(id):
-    for i in my_posts:
-        if i["id"] == id:
-            return i
-
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p["id"] == id:
-            return i
+app.include_router(post.router)
+app.include_router(user.router)
+app.include_router(auth.router)
+app.include_router(like.router)
 
 
 @app.get("/")
 def root():
     return {"messages": "HelloWorld"}
-
-@app.get("/posts")
-def get_posts():
-    return{"data": my_posts}
-
-@app.post("/posts", status_code=HTTP_201_CREATED)
-def create_posts(post: Post):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0, 100000)
-    my_posts.append(post_dict)
-    return{"data": post_dict}
-
-@app.get("/posts/{id}")
-def get_post(id: int, response: Response):
-    post = find_post(int(id))
-
-    if not post:
-        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-        #response.status_code = status.HTTP_404_NOT_FOUND
-        #return{"msg": f"post with id: {id} was not found"}
-
-    return {"post_detail": post}
-
-
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_posts(id: int):
-    #deleting posts
-    #find the index of the posts that should be deleted
-    index = find_index_post(id)
-    if index == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with {id} does not exist")
-    my_posts.pop(index)
-
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-@app.put("/posts/{id}")
-def update_post(id: int, post: Post):
-
-    index = find_index_post(id)
-    
-    if index == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with {id} does not exist")
-
-    post_dict = post.dict()
-    post_dict['id'] = id
-    my_posts[index] = post_dict
-    return{"data": post_dict}
