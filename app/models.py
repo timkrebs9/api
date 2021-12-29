b8575dfd79c3a263c759489f70eea33371e40192
tim-krebs <timkrebs9@gmail.com>
2021-12-29 09:43:38 +0100

    modified userf model

--- app/models.py
+++ app/models.py
@@ -14,7 +14,7 @@ class Post(Base):
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
-    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
 
     owner = relationship("User")
 
