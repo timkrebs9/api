0c640924d5b7997beadb6f9b56e6712ab78a54b9
tim-krebs <timkrebs9@gmail.com>
2021-12-30 12:10:23 +0100

    added PostOut-Class

--- app/schemas.py
+++ app/schemas.py
@@ -18,11 +18,20 @@ class PostCreate(PostBase):
 class PostUpdate(PostBase):
     pass
 
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
 
 class Post(PostBase):
     id: int
     created_at: datetime
-    user_id: int
+    owner_id: int
+    owner: UserOut
 
     class Config:
         orm_mode = True
@@ -30,7 +39,7 @@ class Post(PostBase):
 
 class PostOut(BaseModel):
     Post: Post
-    votes: int
+    likes: int
 
     class Config:
         orm_mode = True
