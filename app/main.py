b5ab41f3d4f0c5c43ecd9aa5933fb7522c7d50a4
tim-krebs <timkrebs9@gmail.com>
2021-12-30 13:29:12 +0100

    connected to database

--- app/main.py
+++ app/main.py
@@ -9,8 +9,6 @@ from .config import settings
 models.Base.metadata.create_all(bind=engine)
 
 
-
-
 app = FastAPI()
 
 origins = ["*"]
@@ -27,6 +25,7 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(like.router)
+
 
 
 @app.get("/")
